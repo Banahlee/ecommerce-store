2a31c2e3720ed1da2e392dc7f1f19d8fd84fdd84
banah <banah.lee@gmail.com>
2019-11-08 10:28:40 -0700

    updated paypal and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import {ButtonContainer} from './Button'
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+                <NavWrapper className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
                     <ul className='navbar-nav align-items-center'>
-                        <li className='navbar-nav ml-5'>
-                            <Link to = '/'>Home</Link>
+                        <li className='nav-item ml-5'>
+                            <Link to = '/' className='nav-link'>Home</Link>
                         </li>
                     </ul>
                     
                     <Link to='/cart' className='ml-auto'>
-                        <button>
-                            <i className='fas fa-cart-plus' />
+                        <ButtonContainer>
+                            <span className='mr-2'>
+                                <i className='fas fa-cart-plus' />
+                            </span>            
                             view cart
-                        </button>
+                        </ButtonContainer>
                     </Link>
                    
-                </nav>
-            </div>
+                </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue) !important;
+    .nav-link{
+        color:var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
